711507676bc0aa914fb04ecfe381030e069ab8e0
Suzanne Corporaal <suzannecorporaal@gmail.com>
2023-12-05 13:45:29 +0100

    implement rudimentary solution for part 2

--- src/d5.js
+++ src/d5.js
@@ -2,49 +2,63 @@ const fs = require("fs");
 
 function readValuesFromFile() {
     const fileBuffer = fs.readFileSync("../inputs/d5.txt", { encoding: "utf-8" });
-    return fileBuffer.trim().split(/[\r]?\n[\r]?\n/g);
+    return fileBuffer.trim().split(/\r?\n\r?\n/);
 }
 
 const mapValues = readValuesFromFile();
-const seeds = mapValues
-    .shift()
-    .split(": ")[1]
-    .split(" ")
-    .map(v => +v);
+const seedValues = mapValues.shift().split(": ")[1].split(" ").map(Number);
+const seedRanges = getSeedRanges(seedValues);
 const maps = mapValues.map(m => {
-    const current = m.split(/\r\n/);
-    const tmp = { mapType: current.shift(), ranges: [] };
-    for (const value in current) {
-        [destinationStart, sourceStart, range] = current[value].split(" ").map(v => +v);
-        tmp.ranges.push({ destinationStart, sourceStart, range });
-    }
-    return tmp;
+    const [mapType, ...ranges] = m.split(/\r?\n/);
+    return {
+        mapType,
+        ranges: ranges.map(line => {
+            const [destinationStart, sourceStart, range] = line.split(" ").map(Number);
+            return { destinationStart, sourceStart, range };
+        })
+    };
 });
+
+function getSeedRanges(seedValues) {
+    const seedRanges = [];
+    for (let i = 0; i < seedValues.length; i += 2) {
+        seedRanges.push({ start: seedValues[i], end: seedValues[i] + seedValues[i + 1] });
+    }
+    return seedRanges;
+}
 
 function valueIsInRange({ sourceStart, range }, val) {
     return val >= sourceStart && val < sourceStart + range;
 }
 
 function convertValue(val) {
-    let newVal = val;
-    for (const i in maps) {
-        const ranges = maps[i].ranges;
-        const tmp = newVal;
-        for (const j in ranges) {
-            const { destinationStart, sourceStart, range } = ranges[j];
-            if (valueIsInRange(ranges[j], newVal)) {
-                const diff = newVal - sourceStart;
-                newVal = destinationStart + diff;
+    for (const { ranges } of maps) {
+        for (const { destinationStart, sourceStart, range } of ranges) {
+            if (valueIsInRange({ sourceStart, range }, val)) {
+                const diff = val - sourceStart;
+                val = destinationStart + diff;
                 break;
             }
         }
     }
-    return newVal;
+    return val;
 }
 
 function calculatePart1() {
-    const locationValues = seeds.map(s => convertValue(s)).sort((a, b) => a - b);
-    return locationValues[0];
+    return seedValues.map(convertValue).sort().shift();
+}
+
+function calculatePart2() {
+    let lowestVal = Infinity;
+    for (let i in seedRanges) {
+        const { start, end } = seedRanges[i];
+        for (let j = start; j <= end; j++) {
+            const location = convertValue(j);
+            lowestVal = Math.min(lowestVal, location);
+        }
+    }
+    return lowestVal;
 }
 
 console.log("part 1: ", calculatePart1());
+console.log("part 2: ", calculatePart2());
