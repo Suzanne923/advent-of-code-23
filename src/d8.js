9b4c616ac6976a9632455acd34c7b87723370b52
Suzanne Corporaal <suzannecorporaal@gmail.com>
2023-12-08 11:58:21 +0100

    implement day 8 part 2

--- src/d8.js
+++ src/d8.js
@@ -8,26 +8,18 @@ function readValuesFromFile() {
 }
 
 const START = "AAA";
-const END = "ZZZ";
 
 function findPath(position = START, paths, instructions) {
     let steps = 0;
     let turn;
 
-    while (position !== END) {
+    while (position[2] !== "Z") {
         turn = instructions[steps % instructions.length];
         steps++;
         position = paths[position][turn === "L" ? 0 : 1];
     }
 
     return steps;
-}
-
-function calculatePart1() {
-    const input = readValuesFromFile();
-    const [instructions, ,] = input.splice(0, 2);
-    const paths = mapInputToPaths(input);
-    return findPath(START, paths, instructions);
 }
 
 function mapInputToPaths(input) {
@@ -42,12 +34,31 @@ function mapInputToPaths(input) {
         }, {});
 }
 
-function calculatePart2() {}
+function leastCommonDivisor() {
+    const gcd = (a, b) => (a ? gcd(b % a, a) : b);
+    const lcm = (a, b) => (a * b) / gcd(a, b);
+    return lcm;
+}
+
+function calculatePart1() {
+    const input = readValuesFromFile();
+    const [instructions, ,] = input.splice(0, 2);
+    const paths = mapInputToPaths(input);
+    return findPath(START, paths, instructions);
+}
+
+function calculatePart2() {
+    const input = readValuesFromFile();
+    const [instructions, ,] = input.splice(0, 2);
+    const paths = mapInputToPaths(input);
+    const startPositions = Object.keys(paths).filter(path => path[2] === "A");
+    return startPositions.map(position => findPath(position, paths, instructions)).reduce(leastCommonDivisor());
+}
 
 console.time("execution time");
 const part1Result = calculatePart1();
-// const part2Result = calculatePart2();
+const part2Result = calculatePart2();
 console.timeEnd("execution time");
-console.log("part 1:", part1Result); // 247823654
-// console.log("part 2:", part2Result); // 245461700
-// 43.843ms
+console.log("part 1:", part1Result); // 11567
+console.log("part 2:", part2Result); // 9858474970153
+// 32.487ms
