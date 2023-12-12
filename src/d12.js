dae1714fc46e9c5cb65950995fa349bfbe768023
Suzanne Corporaal <suzannecorporaal@gmail.com>
2023-12-12 20:07:20 +0100

    implement day 12 part 2

--- src/d12.js
+++ src/d12.js
@@ -8,46 +8,60 @@ function readValuesFromFile() {
 }
 
 const records = readValuesFromFile();
-
-function getSum(numbers) {
-    return numbers.reduce((acc, curr) => acc + curr, 0);
-}
+const memo = new Map();
 
 function calculateArrangements(lineData, groups) {
-    if (groups.length === 0) {
-        // if there are any unmatched "#" left, the arrangement is invalid
-        return lineData.length === 0 || !lineData.includes("#") ? 1 : 0;
+    const memoKey = lineData + "-" + groups.join(",");
+    if (memo.has(memoKey)) {
+        return memo.get(memoKey);
     }
 
-    if (lineData.length < getSum(groups) + groups.length - 1) {
+    if (groups.length === 0) {
+        // if there are any unmatched "#" left, the arrangement is invalid
+        const result = lineData.length === 0 || !lineData.includes("#") ? 1 : 0;
+        memo.set(memoKey, result);
+        return result;
+    }
+
+    if (lineData.length < groups.reduce((acc, group) => acc + group, 0) + groups.length - 1) {
         // not enough line data to match contiguous groups, invalid arrangement
+        memo.set(memoKey, 0);
         return 0;
     }
 
     if (lineData[0] === ".") {
-        return calculateArrangements(lineData.slice(1), groups);
+        const result = calculateArrangements(lineData.slice(1), groups);
+        memo.set(memoKey, result);
+        return result;
     }
 
     if (lineData[0] === "#") {
         const [group, ...remainingGroups] = groups;
+
         if (lineData.slice(0, group).includes(".")) {
             // found ".", line data didn't match contiguous group, invalid arrangement;
+            memo.set(memoKey, 0);
             return 0;
         }
 
         if (lineData[group] === "#") {
             // contiguous group was larger than group to match, invalid arrangement;
+            memo.set(memoKey, 0);
             return 0;
         }
 
         // found matching number of "#" for contiguous group, continue with remaining data;
-        return calculateArrangements(lineData.slice(group + 1), remainingGroups);
+        const result = calculateArrangements(lineData.slice(group + 1), remainingGroups);
+        memo.set(memoKey, result);
+        return result;
     }
 
-    // each ? can be either # or ., count arrangements for both possibilities
-    return (
-        calculateArrangements("#" + lineData.slice(1), groups) + calculateArrangements("." + lineData.slice(1), groups)
-    );
+    // each ? can be either "#" or ".", count arrangements for both possibilities
+    const result =
+        calculateArrangements("#" + lineData.slice(1), groups) + calculateArrangements("." + lineData.slice(1), groups);
+
+    memo.set(memoKey, result);
+    return result;
 }
 
 function getSumOfArrangements(copies) {
@@ -69,8 +83,8 @@ function calculatePart2() {
 
 console.time("execution time");
 const part1Result = calculatePart1();
-// const part2Result = calculatePart2();
+const part2Result = calculatePart2();
 console.timeEnd("execution time");
 console.log("part 1:", part1Result); // 7843
-// console.log("part 2:", part2Result); //
-//
+console.log("part 2:", part2Result); // 10153896718999
+// 496.348ms
